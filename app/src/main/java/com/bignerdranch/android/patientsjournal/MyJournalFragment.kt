5efa280acf7c2225ccf53d07abed5d63971622d5
ÐFarma20 <kustov.ius@students.dvfu.ru>
2023-01-18 20:12:36 +1000

    Реализован слайдер для определения снижения боли. Изменение поожения слайдера меняют показания в TextView. Добавленны гетеры и сетеры в ModelView для работы с painPercents. Добавлена новая переменная в класс локальной базы данных

--- app/src/main/java/com/bignerdranch/android/patientsjournal/MyJournalFragment.kt
+++ app/src/main/java/com/bignerdranch/android/patientsjournal/MyJournalFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -18,6 +19,8 @@ const val REQUEST_PROGRAM = 0
 class MyJournalFragment: Fragment(), ProgramSelectionDialogFragment.Callbacks {
 
     private lateinit var programSelectionButton: TextView
+    private lateinit var percentBarTextView: TextView
+    private lateinit var painSeekBar: SeekBar
 
     //Ленивая инициализация ViewModel
     private val myJournalViewModel: MyJournalViewModel by lazy {
@@ -40,6 +43,8 @@ class MyJournalFragment: Fragment(), ProgramSelectionDialogFragment.Callbacks {
         val view = inflater.inflate(R.layout.fragment_myjournal, container, false)
 
         programSelectionButton = view.findViewById(R.id.program_selected_button) as TextView
+        percentBarTextView = view.findViewById(R.id.painPercentsBar) as TextView
+        painSeekBar = view.findViewById(R.id.painSeekBar) as SeekBar
 
         return view
     }
@@ -58,6 +63,21 @@ class MyJournalFragment: Fragment(), ProgramSelectionDialogFragment.Callbacks {
                     show(this@MyJournalFragment.requireFragmentManager(), PROGRAM_SELECTION_TAG)
                 }
         }
+
+
+        percentBarTextView.text = "${myJournalViewModel.getPainLevel()}"
+
+        //Слушатель на painSeekBar
+        painSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            @SuppressLint("SetTextI18n")
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val percents = progress * 5
+                percentBarTextView.text = "$percents%"
+                myJournalViewModel.setPainLevel(percents)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {    }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {    }
+        })
     }
 
     @SuppressLint("SetTextI18n")
--- app/src/main/java/com/bignerdranch/android/patientsjournal/MyJournalViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/patientsjournal/MyJournalViewModel.kt
@@ -7,9 +7,20 @@ class MyJournalViewModel: ViewModel() {
     //переменная, хранящая в себе данные ответов
     val patientsAnswer = PatientsAnswer()
 
-    //Фукнкция для смены режима программы
-    fun changeProgramMode(id: Int){
+    //Гетеры и сетеры для режима программы
+    fun setProgramMode(id: Int){
         patientsAnswer.program = id
+    }
+    fun getProgramMode(): Int {
+        return patientsAnswer.program
+    }
+
+    //Гетеры и сетеры для уровня боли
+    fun setPainLevel(percents: Int){
+        patientsAnswer.painBar = percents
+    }
+    fun  getPainLevel(): Int{
+        return patientsAnswer.painBar
     }
 
 }
