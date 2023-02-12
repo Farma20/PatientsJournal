d5597d19ffc941db0725e258040fb2dc2793550f
ÐFarma20 <kustov.ius@students.dvfu.ru>
2023-02-12 22:42:18 +1000

    Создано представление для отметок на человеке

--- app/src/main/java/com/bignerdranch/android/patientsjournal/BodyEditorFrontFragment.kt
+++ app/src/main/java/com/bignerdranch/android/patientsjournal/BodyEditorFrontFragment.kt
@@ -6,23 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 
 class BodyEditorFrontFragment: Fragment() {
 
     private lateinit var hostActivity: MainActivity
 
-    private lateinit var left_hand: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_body_editor_front, container, false)
+        val view = inflater.inflate(R.layout.fragment_body_editor, container, false)
 
-        left_hand = view.findViewById(R.id.left_hand) as ImageView
 
         return view
     }
@@ -31,14 +28,6 @@ class BodyEditorFrontFragment: Fragment() {
     override fun onStart() {
         super.onStart()
 
-        left_hand.setOnClickListener{
-            it.alpha = if (it.alpha == 0.0f){
-                1.0f
-            }else{
-                0.0f
-            }
-            println("alpha is 1 ${it.alpha}")
-        }
     }
 
     override fun onAttach(context: Context) {
